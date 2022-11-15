824a35ae7606492380c767485f187ea986b34177
YuriAdrian41 <donaldlafranchi@gmail.com>
2022-11-16 02:07:32 +0900

    Implementing Time Travel

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import './App.css';
 
 const App = () => {
   const [history, setHistory] = useState([{ squares: Array(9).fill(null) }]);
+  const [stepNumber, setStepNumber] = useState(0);
   const [xIsNext, setXIsNext] = useState(true);
 
   const calculateWinner = (tempSquares: any) => {
@@ -28,18 +29,33 @@ const App = () => {
   };
 
   const handleClick = (i: any) => {
-    const current = history[history.length - 1];
+    const tempHistory = history.slice(0, stepNumber + 1);
+    const current = tempHistory[tempHistory.length - 1];
     const squares = current.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
       return;
     }
     squares[i] = xIsNext ? 'X' : 'O';
-    setHistory(history.concat([{ squares }]));
+    setHistory(tempHistory.concat([{ squares }]));
+    setStepNumber(tempHistory.length);
     setXIsNext(!xIsNext);
   }
 
-  const current = history[history.length - 1];
+  const jumpTo = (step: any) => {
+    setStepNumber(step);
+    setXIsNext((step % 2) === 0);
+  }
+
+  const current = history[stepNumber];
   const winner = calculateWinner(current.squares);
+
+  const moves = history.map((step, move) => {
+    const desc = move ? `Go to move #${move}` : 'Go to game start';
+    return (<li key={move}>
+      <button onClick={() => jumpTo(move)}>{desc}</button>
+    </li>);
+  });
+
   let status;
   if (winner) {
     status = 'Winner: ' + winner;
@@ -54,7 +70,7 @@ const App = () => {
       </div>
       <div className='game-info'>
         <div>{status}</div>
-        <div>{/* TODO */}</div>
+        <ol>{moves}</ol>
       </div>
     </div>
   );
