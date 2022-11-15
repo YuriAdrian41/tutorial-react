5f99465182053d498453d1f4853f66c76d1427c2
YuriAdrian41 <donaldlafranchi@gmail.com>
2022-11-16 01:34:09 +0900

    Declaring a Winner

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -6,8 +6,31 @@ const Board = () => {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [xIsNext, setXIsNext] = useState(true);
 
+  const calculateWinner = (tempSquares: any) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (tempSquares[a] && tempSquares[a] === tempSquares[b] && tempSquares[a] === tempSquares[c]) {
+        return tempSquares[a];
+      }
+    }
+    return null;
+  };
+
   const handleClick = (i: any) => {
     const tempSquares = squares.slice();
+    if (calculateWinner(tempSquares) || tempSquares[i]) {
+      return;
+    }
     tempSquares[i] = xIsNext ? 'X' : 'O';
     setSquares(tempSquares);
     setXIsNext(!xIsNext);
@@ -17,7 +40,13 @@ const Board = () => {
     return <Square value={squares[i]} onClick={() => handleClick(i)} />;
   };
 
-  const status = `Next player: ${xIsNext ? 'X' : 'O'}`;
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = 'Winner: ' + winner;
+  } else {
+    status = `Next player: ${xIsNext ? 'X' : 'O'}`;
+  }
 
   return (<div>
     <div className="status">{status}</div>
